3757bbe14ed6188b45651e163e3250106be2c60d
ephraim <ephraims.818@gmail.com>
2026-03-02 16:52:02 +0600

    REACT--COMPLE-GUIDE -- REACT-ESSENTIALS -- 05. Alternative Props Syntaxes

--- 01. React Essentials/react-basic/src/App.jsx
+++ 01. React Essentials/react-basic/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 function genRandomInt(max) {
@@ -21,12 +21,12 @@ function Header() {
     );
 }
 
-function CoreConcept(props) {
+function CoreConcept({ title, description, image }) {
     return (
         <li>
-            <img src={props.img} alt="" />
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
         </li>
     );
 }
@@ -39,14 +39,15 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept
-                            title="Components"
-                            description="The core UI building block."
-                            img={componentsImg}
-                        />
-                        <CoreConcept title="Props" />
-                        <CoreConcept />
-                        <CoreConcept />
+                        {/* <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            img={CORE_CONCEPTS[0].image}
+                        /> */}
+                        <CoreConcept {...CORE_CONCEPTS[0]} />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
                 </section>
             </main>
